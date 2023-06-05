aedb80935a1d290eeeafe191b182362a109ee5eb
Alexander Ioffe <deusaquilus@gmail.com>
2023-06-05 01:18:26 -0400

    Bump version for next release

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ apply(plugin = "io.github.gradle-nexus.publish-plugin")
 allprojects {
 
   group = "io.exoquery"
-  version = "0.0.1"
+  version = "0.0.2"
 
   apply(plugin = "kotlin")
   apply(plugin = "maven-publish")
