a445cf8cce9b8e46d2d53cd6afb341ebb2d10ea2
Alexander Ioffe <deusaquilus@gmail.com>
2024-07-18 12:37:05 -0400

    Simplify the bug

--- decomat-examples/src/commonMain/kotlin/io/decomat/examples/Data.kt
+++ decomat-examples/src/commonMain/kotlin/io/decomat/examples/Data.kt
@@ -2,23 +2,10 @@ package io.decomat.examples
 
 import io.decomat.*
 
-sealed interface Name
 @Matchable
-data class SimpleName(@Component val first: String, @Component val last: String): Name, HasProductClass<SimpleName> {
+data class FullName(@Component val first: String, val middle: String, @Component val last: String): HasProductClass<FullName> {
   override val productComponents = productComponentsOf(this, first, last)
   companion object { }
-}
-
-@Matchable
-data class FullName(@Component val first: String, val middle: String, @Component val last: String): Name, HasProductClass<FullName> {
-  override val productComponents = productComponentsOf(this, first, last)
-  companion object { }
-}
-
-@Matchable
-data class Person(@Component val name: Name, @Component val age: Int): HasProductClass<Person> {
-  override val productComponents = productComponentsOf(this, name, age)
-  companion object {}
 }
 
 fun usingGen(): String {
