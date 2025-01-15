362512f662566058b20d218919a3ce0296a03647
Alexander Ioffe <deusaquilus@gmail.com>
2025-01-15 00:18:45 -0500

    Fixing up calls

--- decomat-examples/src/commonTest/kotlin/io/decomat/examples/CustomPatternTestData.kt
+++ decomat-examples/src/commonTest/kotlin/io/decomat/examples/CustomPatternTestData.kt
@@ -8,7 +8,7 @@ import io.decomat.examples.customPatternData1.*
 // TODO move to commonTest when KT-70025 is fixed
 object FirstName2 {
   operator fun get(nameString: Pattern0<String>) =
-    customPattern1(nameString) { it: Name ->
+    customPattern1("FirstName2", nameString) { it: Name ->
       when(it) {
         is SimpleName -> Components1(it.first)
         is FullName -> Components1(it.first)
--- decomat-examples/src/commonTest/kotlin/io/decomat/examples/CustomPatternTestData2.kt
+++ decomat-examples/src/commonTest/kotlin/io/decomat/examples/CustomPatternTestData2.kt
@@ -9,7 +9,7 @@ import io.decomat.*
 // TODO move to commonTest when KT-70025 is fixed
 object FirstLast {
   operator fun get(first: Pattern0<String>, last: Pattern0<String>) =
-    customPattern2(first, last) { it: Name ->
+    customPattern2("FirstLast", first, last) { it: Name ->
       on(it).match(
         case(FullName[Is(), Is()]).then { first, last -> Components2(first, last) },
         case(SimpleName[Is(), Is()]).then { first, last -> Components2(first, last) }
